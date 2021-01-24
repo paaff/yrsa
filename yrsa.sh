b5dd603498a93259e4085b65a195514b3d3aafb1
paaff <pafischer1992@gmail.com>
2021-01-24 18:39:01 +0100

    added figlet title and msg for files found status

--- yrsa.sh
+++ yrsa.sh
@@ -1,27 +1,33 @@
 #!/usr/bin/env bash
 
 # Welcome
-echo 'YRSA'
+figlet 'YRSA'
 
-# fun:
+# fun list():
 #	Listing media files in the dir and all subfolders.
 #	Putting the paths in a variable.
 #
-function list () {
+list () {
 	files=$(find $dir -name "*.mkv" -type f)
-	echo $files
+	
+	if [[ -z "$files" ]]; then
+		echo 'yrsa found no ".mkv" files here.'
+	elif [[ -n "$files" ]]; then
+		echo 'yrsa found the following ".mkv" files.'
+		for i in "${files[@]}"; do
+			echo "$i"
+		done
+	fi
 	return 0
 }
 
-# fun:
+# fun convert():
 #	Converting the discovered media files.
 #
-
-function convert () {
+convert () {
 	for i in "${files[@]}"; do
-		echo "$i"
+		ffmpeg -i movie_dts.mkv -map 0:v -map 0:a:0 -map 0:a -map 0:s -c:v copy -c:a copy -c:s copy -c:a:0 aac -b:a:0 640k movie-aac.mkv
 	done
 	return 0
 }
 list
-convert
